8f36e6880071c58b6c5c8d767773b3470243c401
TheJetlag <TheJetlag@users.noreply.github.com>
2015-11-30 16:14:00 +0100

    fix renamer

--- renamer.js
+++ renamer.js
@@ -1,10 +1,10 @@
 use tls;
-db.tls.find({}).forEach(function(item)
+db.scans.find({}).forEach(function(item)
 {
     for(i = 0; i < item.ciphers.length; i++)
     {
         item.ciphers[i].kxStrength = item.ciphers[i].kxStrenght;
-        delete item.ciphers[i].kxStrength;
+        delete item.ciphers[i].kxStrenght;
     }
     db.Setting.update({_id: item._id}, item);
 });
